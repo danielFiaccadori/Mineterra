edb76049e3725842f121442271605d4ca956b9f5
danielFiaccadori <danie@Estdio>
2023-12-01 13:30:24 -0300

    Bleeding Bug Corrected

--- src/main/java/net/mcreator/mineterra/procedures/SangramentoOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/mineterra/procedures/SangramentoOnEffectActiveTickProcedure.java
@@ -1,6 +1,7 @@
 package net.mcreator.mineterra.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
@@ -14,8 +15,10 @@ public class SangramentoOnEffectActiveTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)), 1);
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles((SimpleParticleType) (MineterraModParticleTypes.BLOOD.get()), x, y, z, 30, 1, (-1), 1, 0);
+		if (!((entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1) == 0)) {
+			entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)), 1);
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles((SimpleParticleType) (MineterraModParticleTypes.BLOOD.get()), x, y, z, 30, 1, (-1), 1, 0);
+		}
 	}
 }
